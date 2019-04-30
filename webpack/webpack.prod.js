a1abd63fad95b30d13f71b6b7c1e0dc50789b3ee
zhazhonglin <ex-zhazhonglin@pingan.com.cn>
2019-04-30 15:00:52 +0800

    up

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -2,8 +2,19 @@ const merge = require('webpack-merge');
 const baseConfig = require('./webpack.base');
 const path = require('path');
 const webpack = require('webpack');
-
+const AutoDllPlugin = require('autodll-webpack-plugin');
 module.exports = merge(baseConfig, {
     devtool: 'source-map', // 压缩方式
-    mode: 'production'
+    mode: 'production',
+    plugins: [
+        new AutoDllPlugin({
+            inject: true, // will inject the DLL bundles to index.html
+            filename: '[name].js',
+            entry: {
+              vendor: [
+                'vue',
+              ]
+            }
+          })
+    ]
 })
