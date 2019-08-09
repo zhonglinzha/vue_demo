be16ad2efca305c5c1fb3a90eb32d66f203eea32
zhonglin.zha <774640601@qq.com>
2019-08-10 07:37:48 +0800

    up

--- src/base/index.js
+++ src/base/index.js
@@ -1,11 +1,30 @@
-import 'babel-polyfill';
-import Vue from 'vue'
-import App from './app'
-import router from './router'
-Vue.config.productionTip = false;
+import Vue from 'vue';
+import App from './app';
+import VueLazyLoad from 'vue-lazyload';
 
-new Vue({
+Vue.use(VueLazyLoad)
+
+Vue.directive('test', {
+    bind: function(el,binding,vnode,oldVnode){
+        console.log('------1-------');
+    },
+    inserted: function(el,binding,vnode,oldVnode){
+        console.log('------2-------');
+    },
+    update: function(el,binding,vnode,oldVnode){
+        console.log('------3-------');
+    },
+    componentUpdated: function(el,binding,vnode,oldVnode){
+        console.log('------4-------');
+    },
+    unbind: function(el,binding,vnode,oldVnode){
+        console.log('------5-------');
+    }
+});
+
+const vm = new Vue({
     el: "#app",
     router,
     render: (h) => h(App)
 })
+
